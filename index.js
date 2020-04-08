21283e0f01145ae27fb90a3a97532795631bd0b4
YasirHasn9 <hassanyasir223@gmail.com>
2020-04-07 23:03:38 -0700

    handler function to get users

--- index.js
+++ index.js
@@ -2,9 +2,15 @@
 // its like a react for js
 const express = require("express");
 
+// every app in real world needs data that can the client interact with it
+// this is a fake data
+const db = require("./database");
+
+// there is 2 name that standardized  with calling the express either App or Server
 const app = express();
 
 // parse int so it work as an object
+// because the express deals with string
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -13,6 +19,16 @@ app.get("/", (req, res) => {
   });
 });
 
+app.get("/users", (req, res) => {
+  const users = db.getUsers();
+  if (users) {
+    return res.status(200).json(users);
+  } else {
+    return res.status(404).send({
+      message: "Not Found"
+    });
+  }
+});
 app.listen(8080, () => {
   console.log("Listening at http://localhost:8080");
 });
